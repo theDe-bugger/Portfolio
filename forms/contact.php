8e37ff53a1f3394bacad962ddd5be91c5476ac91
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-03-31 17:52:02 -0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
 
   $receiving_email_address = 'jaimildalwadi2003@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
